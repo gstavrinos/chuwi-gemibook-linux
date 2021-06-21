f75f86dc98259f1c542c1c844cde83c0effa062a
George Stavrinos <gstavrinos@protonmail.com>
2021-06-21 13:42:29 +0300

    Added the wifi (hardlock) fix script

---
+++ wifi_fix.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install rtl8821ce-dkms
+echo "blacklist rtw88_8821ce" | sudo tee -a /etc/modprobe.d/dkms.conf
+echo "blacklist rtl8821ce" | sudo tee -a /etc/modprobe.d/dkms.conf
+sudo update-initramfs -u
