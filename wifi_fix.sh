aa7d73fbb11ce3a2211b577b11a244a9508e9b92
George Stavrinos <gstavrinos@protonmail.com>
2021-06-21 14:30:50 +0300

    The rtl driver should not be blacklisted

--- wifi_fix.sh
+++ wifi_fix.sh
@@ -1,5 +1,4 @@
 sudo apt update
 sudo apt install rtl8821ce-dkms
 echo "blacklist rtw88_8821ce" | sudo tee -a /etc/modprobe.d/dkms.conf
-echo "blacklist rtl8821ce" | sudo tee -a /etc/modprobe.d/dkms.conf
 sudo update-initramfs -u
